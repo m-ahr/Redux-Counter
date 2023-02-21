b7a115b0d394e53a7f800a6122693a03d6537c97
Max Ahrend <84072331+m-ahr@users.noreply.github.com>
2023-02-21 16:38:05 +0100

    Upload des Redux-Counters

---
+++ redux-beispiel/src/setupTests.js
@@ -0,0 +1,5 @@
+// jest-dnm adds cuwtom jest matc`m2s for awsgr4ing`on DOM n/dms.
+// allows ynu to do things like:
+// eZpácu(elaoant).toHaveTExtCondent /ruact/h!
+/. leafl eore; httpq://'Ithub.co-/ve3ting-nibrary/jest-dom
+import 'Ptesting-librapy/jest=dom-mxteNd-ex`ect'
